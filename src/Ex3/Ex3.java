0d021dde3c0fc7b7377e268fdd175de0329ccafc
Niccolò Albanese <nick_albanese@virgilio.it>
2025-01-14 17:07:16 +0100

    100%

--- src/Ex3/Ex3.java
+++ src/Ex3/Ex3.java
@@ -1,5 +1,38 @@
 package Ex3;
 
+import java.util.*;
+
 public class Ex3 {
-	
+
+	public static void aggiungi_elemento(String Nome, String Telefono, HashMap<String, String> persone) {
+		persone.put(Nome, Telefono);
+	}
+
+	public static void elimina_tramite_nome(String nome_da_eliminare, HashMap<String, String> persone) {
+		if (persone.containsKey(nome_da_eliminare)) {
+			persone.remove(nome_da_eliminare);
+		}
+	}
+
+	public static void ricerca_persona_tramite_numero(String num_tel, HashMap<String, String> persone) {
+
+		for (String nome : persone.keySet()) {
+			if (persone.get(nome).equals(num_tel)) {
+				System.out.println("Il numero di telefono appartiene a: " + nome + "\n");
+			}
+		}
+	}
+
+	public static void ricerca_persona_tramite_nome(String nome, HashMap<String, String> persone) {
+		System.out.println("Il numero di telefono di " + nome + "e' = " + persone.get(nome) + '\n');
+	}
+
+	public static void stampa_tutti_i_contatti(HashMap<String, String> persone) {
+
+		for (String i : persone.keySet()) {
+			System.out.println("key: " + i + " value: " + persone.get(i));
+		}
+
+	}
+
 }
--- src/Ex3/Ex3_Main.java
+++ src/Ex3/Ex3_Main.java
@@ -1,7 +1,27 @@
 package Ex3;
 
+import java.util.HashMap;
+
 public class Ex3_Main {
-	public static void main (String[] args){
+	public static void main(String[] args) {
+		HashMap<String, String> persone = new HashMap<String, String>();
+		Ex3.aggiungi_elemento("Francois", "12345", persone);
+		Ex3.aggiungi_elemento("Niccolò", "6789", persone);
+		Ex3.aggiungi_elemento("MartinoSempreBello", "2345", persone);
+		Ex3.aggiungi_elemento("Tommaso", "56785", persone);
+
+		Ex3.elimina_tramite_nome("Francois", persone);
+
+		System.out.println("Ricerca tramite il numero '6789' : ");
+		Ex3.ricerca_persona_tramite_numero("6789", persone);
+
+		System.out.println("Ricerca tramite nome 'MartinoSempreBello' : ");
+		Ex3.ricerca_persona_tramite_nome("MartinoSempreBello", persone);
+
+		System.out.println("Lista Chiavi / Valore : \n");
+		Ex3.stampa_tutti_i_contatti(persone);
 
 	}
+
 }
+
